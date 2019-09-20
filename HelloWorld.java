b7bbc5d2244ebfaf4a49eaf7cd52cb3582dc0bdf
Lucie <bamboomusic0@gmail.com>
2019-09-20 17:38:37 +0200

    ADD QUESTION METHOD

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, Sweng !");
+    System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());
+  }
+  public static String computeUltimateQuestion() {
+    return "still searching...";
   }
 }
