5a440786d1d536e0d06d1c71437963d5951f0e7e
Lucie <bamboomusic0@gmail.com>
2019-09-20 19:25:46 +0200

    merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, Sweng !");
     System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
+    System.out.println("The Guide : branch section test");
   }
   public static int computeUltimateAnswer() {
 	return 6 * 7;
